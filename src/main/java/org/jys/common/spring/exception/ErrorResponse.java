1cd43fe50af770459331a1c724e7633f88cb08a8
jiangyuesong <1259012537@qq.com>
2019-03-16 10:20:00 +0800

    add spring exception handler example

---
+++ src/main/java/org/jys/common/spring/exception/ErrorResponse.java
@@ -0,0 +1,42 @@
+package org.jys.common.spring.exception;
+
+/**
+ * @author YueSong Jiang
+ * @date 2019/3/16
+ * @description <p> </p>
+ */
+public class ErrorResponse {
+
+    private int errorCode;
+
+    private String message;
+
+    public ErrorResponse() {
+
+    }
+
+    public ErrorResponse(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ErrorResponse(int errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
