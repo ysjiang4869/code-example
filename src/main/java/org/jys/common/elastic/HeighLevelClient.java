c23633aa8f9a7a8e468eb52142be8592b9ae5a3e
jiangyuesong <1259012537@qq.com>
2019-03-25 19:10:25 +0800

    add elastic search demo

---
+++ src/main/java/org/jys/common/elastic/HeighLevelClient.java
@@ -0,0 +1,23 @@
+package org.jys.common.elastic;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+/**
+ * @author YueSong Jiang
+ * @date 2019/3/21
+ * @description <p> </p>
+ */
+public class HeighLevelClient {
+
+    private RestHighLevelClient client;
+    public HeighLevelClient() {
+
+        client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("192.168.14.8", 9200, "http"),
+                        new HttpHost("192.168.14.10", 9200, "http"),
+                        new HttpHost(new HttpHost("192.168.14.12", 9200, "http"))));
+    }
+}
